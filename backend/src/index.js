0fe39bac9c4ae6084eff5cd8abf14b88f8dac176
CamWang <camwang@outlook.com>
2021-08-04 18:08:28 +0800

    feat[be]: judge message push

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const httpServer = http.createServer(app);
+const server = http.createServer(app);
 const { Server } = require('socket.io');
-const io = new Server(httpServer);
+const io = new Server(server);
 
 const port = 3000;
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 app.use(express.static('dist'));
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+app.post('/submit', function(req, res){
+  let uid = req.body.uid;
+  res.json({ status: "success", type:"message", message: "submitted" });
+  setTimeout(function() {
+    let obj = {
+      success: true,
+      title: "Compile Success",
+    }
+    io.to(uid).emit("judge compile", JSON.stringify(obj));
+  }, 5000);
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log(`${socket.id} connected`);
+  io.on('disconnect', () => {
+    console.log(`${socket.id} disconnected`);
+  });
+});
+
+server.listen(port, () => {
   console.log(`Cloud IDE listening at http://localhost:${port}`)
 })
